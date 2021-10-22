ca4b10644bf3aa7368f30bb965ee58b702382301
Paulo Victor Capuano <pvcapuano@gmail.com>
2021-10-22 21:51:37 +0000

    create list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-
 import React from "react"
+import { useState } from "react"
+import { Item } from "./types/Item"
 import { Container, Area, Header } from "./App.styles"
 
 const App = () => {
+
+  const [ list, setList ] = useState<Item[]>([
+    {id: 1, name: "Macarrão", done: false},
+    {id: 2, name: "Farinha de trigo", done: true},
+  ])
   return(
     
       <Container> 
         <Area> 
           <Header> Lista de tarefas </Header>
+
+          {list.map((item, index) => {
+
+          })}
+
         </Area>
        </Container>
     
