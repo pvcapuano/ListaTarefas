312d881792e4b734678d47b1f523b085da30491d
Paulo Victor Capuano <pvcapuano@gmail.com>
2021-10-24 13:57:50 +0000

    add state on addarea

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import * as C from "./styles"
+import { useState } from "react"
 
 export const AddArea = () => {
+
+  const [ inputText, setInputText ] = useState("")
+
   return(
     <C.Container>
-    ...
+      <div className="image">➕</div>
+      <input className="text"
+      placeholder="Adicione um item"
+      value={inputText}
+      onChange={e => setInputText(e.target.value)}
+      />
     </C.Container>
   )
 }
